9a7cf1dcf517492543ce1cc11df03d9f994d65af
Chandan Kumar <cksingh8831@outlook.com>
2023-06-05 18:12:27 +0530

    Added complete setup for wWE

--- WWEELB/variables.tf
+++ WWEELB/variables.tf
@@ -1,6 +1,6 @@
-variable "AWS_ACCESS_KEY" {}
+variable "AWS_ACCESS_KEY" {default = "AKIAV2YWTIYVKORWFACV"}
 
-variable "AWS_SECRET_KEY" {}
+variable "AWS_SECRET_KEY" {default = "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"}
 
 variable "AWS_REGION" {
 default = "ap-south-1"
