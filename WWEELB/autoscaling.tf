973b3c05fb6e5b7cdece8b37f2c2fbed4873e501
Chandan Kumar <cksingh8831@outlook.com>
2023-06-05 18:05:48 +0530

    Added userdata in ec2-instance

--- WWEELB/autoscaling.tf
+++ WWEELB/autoscaling.tf
@@ -5,7 +5,28 @@ resource "aws_launch_configuration" "wwealb-launchconfig" {
   instance_type   = "t2.micro"
   key_name        = aws_key_pair.wwealb_key.key_name
   security_groups = [aws_security_group.wwealb-instance.id]
-  user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'Hello Team\nThis is my IP: '$MYIP > /var/www/html/index.html"
+  user_data       = "apt update \apt install openjdk-11-jre-headless \groupadd --system tomcat \useradd -d /usr/share/tomcat -r -s /bin/false -g tomcat tomcat \yum -y install wget \export VER="9.0.75" \wget https://archive.apache.org/dist/tomcat/tomcat-9/v${VER}/bin/apache-tomcat-${VER}.tar.gz \tar xvf apache-tomcat-${VER}.tar.gz -C /usr/share/ \ln -s /usr/share/apache-tomcat-$VER/ /usr/share/tomcat \chown -R tomcat:tomcat /usr/share/tomcat \chown -R tomcat:tomcat /usr/share/apache-tomcat-$VER/ \tee /etc/systemd/system/tomcat.service<<EOF
+[Unit]
+Description=Tomcat Server
+After=syslog.target network.target
+
+[Service]
+Type=forking
+User=tomcat
+Group=tomcat
+
+Environment=JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+Environment='JAVA_OPTS=-Djava.awt.headless=true'
+Environment=CATALINA_HOME=/usr/share/tomcat
+Environment=CATALINA_BASE=/usr/share/tomcat
+Environment=CATALINA_PID=/usr/share/tomcat/temp/tomcat.pid
+Environment='CATALINA_OPTS=-Xms512M -Xmx1024M'
+ExecStart=/usr/share/tomcat/bin/catalina.sh start
+ExecStop=/usr/share/tomcat/bin/catalina.sh stop
+
+[Install]
+WantedBy=multi-user.target
+EOF \systemctl daemon-reload \systemctl start tomcat \systemctl enable tomcat \systemctl status tomcat "
 
   lifecycle {
     create_before_destroy = true
